50ca2e952d97c04584e4ba8f9805f45ea5db2bd3
clalil <cliljander@gmail.com>
2019-08-15 12:42:41 +0200

    View my very first JS code.

---
+++ exercise1.js
@@ -0,0 +1,28 @@
+//Create an array and add three numbers to it.
+myArray = [1, 2, 3];
+//Use your array to return the second number.
+myArray[1]
+//What data type is 123/12? 
+//Answer: A float.
+//What data type is "Things in quotes!"? 
+//Answer: A String.
+//What datatype is undefined?
+//Answer: The meaning of undefined is “value is not assigned” i.e. a variable without a value would have the value "undefined".
+//Write an if statement that returns true.
+if ("This is Javascript" != "This is not like Ruby!") {
+    alert("These two values are NOT equal according to JavaScript!");
+}
+    else {
+        alert("Something strange is happening here...")
+    }
+//Consider these two arrays: myArray = ['Thomas', 'Noel', 'Faraz'] and emptyArray = []. 
+//Use a for loop to add our names to emptyArray.
+let myArray = ['Thomas ', 'Noel ', 'Faraz'];
+let emptyArray = [];
+    for (i = 0; i < myArray.length; i++) {
+        emptyArray += myArray[i];   
+    }
+//Google a helper method for finding the length of an array in Javascript. 
+//Answer: The helper method is called .length.
+//Is it the same as Ruby?
+//Answer: Yes.
