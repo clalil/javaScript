0913bb04dc67e6da58d2a90688b514a6c09d579a
clalil <cliljander@gmail.com>
2019-08-27 16:51:19 +0200

    Add Number.isInteger method to try to pass final test, still fails

--- code_wars5.js
+++ code_wars5.js
@@ -1,9 +1,10 @@
 //Create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
 function filter_list(l) {
-    return l.filter(number => number >= 0);
+    return l.filter(number => Number.isInteger(number) ? l.push(number) : l.pop(number) );
     //Initially I tried to use the map method before realizing everything could be done with the .filter function
     //After reading up on the MDN site I realized that a very short syntax could be declared when naming each item using a local variable (like in |Ruby|)
+    //Final test with multiple arrays is failing, trying to use Number.isInteger(method) as a conditional
   }
   
   filter_list([1,'a','b',0,15])
