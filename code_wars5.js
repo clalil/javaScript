14fb6fb32217711784d2a3d1c79b451c6dd0effc
clalil <cliljander@gmail.com>
2019-08-27 17:07:52 +0200

    Solved final test in CW kata using Math.round

--- code_wars5.js
+++ code_wars5.js
@@ -1,10 +1,8 @@
 //Create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
 function filter_list(l) {
-    return l.filter(number => Number.isInteger(number) ? l.push(number) : l.pop(number) );
-    //Initially I tried to use the map method before realizing everything could be done with the .filter function
-    //After reading up on the MDN site I realized that a very short syntax could be declared when naming each item using a local variable (like in |Ruby|)
-    //Final test with multiple arrays is failing, trying to use Number.isInteger(method) as a conditional
+    return l.filter(number => Math.round(number) === number);
+    //I stumbled across a blog entry going through different Booleans for numeric values and found that Math.round() always returns numbers and === only returns true if both operands have the same type.
   }
   
   filter_list([1,'a','b',0,15])
