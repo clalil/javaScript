ea6a8e00e55cbd515062bbc14312856f7954cc2d
clalil <cliljander@gmail.com>
2019-08-27 15:47:47 +0200

    Add CW kata #5 using .filter method

---
+++ code_wars5.js
@@ -0,0 +1,9 @@
+//Create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+function filter_list(l) {
+    return l.filter(number => number >= 0);
+    //Initially I tried to use the map method before realizing everything could be done with the .filter function
+    //After reading up on the MDN site I realized that a very short syntax could be declared when naming each item using a local variable (like in |Ruby|)
+  }
+  
+  filter_list([1,'a','b',0,15])
