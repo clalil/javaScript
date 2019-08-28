28b28343347428d04cdd414a5ec762106a0bcb2c
clalil <cliljander@gmail.com>
2019-08-28 18:54:15 +0200

    Finalize kata, add kata explanation

--- code_wars8.js
+++ code_wars8.js
@@ -1,5 +1,9 @@
+//In this kata, you are asked to square every digit of a number.    
+
 function squareDigits(num){
     //I want to iterate through the numbers and exponent each one
-      return num.toString().split('').map((num) => Number(num)**2).join('');
-      //In genereal; map((value, index, arrary) => Number(value, index, arrary)); Here however, it works becuase the Number function ignores everything but the first argument.
+      let array = num.toString().split('').map((num) => Number(num)**2)
+      //In genereal; map((value, index, arrary) => Number(value, index, arrary)); However, in a different scenario the map(Number) would have worked becuase the Number function ignores everything but the first argument.
+        return Number(array.join(''));
+        //Because an array was returned by .map, this was one way to turn it back into all integers.
     }
